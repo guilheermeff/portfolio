e8ccbdd70c8a13ef5146e24c490614ad2424a552
Guilherme Fernandes <guilheermeff@outlook.com>
2022-09-22 14:35:57 -0400

    update time reveal

--- js/reveal.js
+++ js/reveal.js
@@ -2,21 +2,21 @@ export function scrollReveal() {
   window.sr = ScrollReveal({reset: true})
 
   ScrollReveal().reveal('.col-a', {
-    Delay: 200
+    Delay: 300
   });
   ScrollReveal().reveal('.col-b', {
-    Delay: 200
+    Delay: 300
   });
   ScrollReveal().reveal('.stats', {
-    Delay: 200
+    Delay: 300
   });
   ScrollReveal().reveal('.card', {
-    Delay: 200
+    Delay: 300
   });
   ScrollReveal().reveal('h2', {
-    Delay: 200
+    Delay: 300
   });
   ScrollReveal().reveal('h4', {
-    Delay: 200
+    Delay: 300
   });
 }
